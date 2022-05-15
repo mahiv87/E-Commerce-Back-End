18c7d41ad6bb7508c19af0d18e412b3ce4abb7c3
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-15 15:09:43 -0600

    added missing return after 404 in category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -60,7 +60,8 @@ router.put('/:id', async (req, res) => {
     )
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this id' })
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
     }
 
     res.status(200).json(categoryData)
