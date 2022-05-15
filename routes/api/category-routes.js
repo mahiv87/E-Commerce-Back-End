7029cd7bafb7c8d26a752f26b8faa458a86870a6
Marcus mbp <maherreraiv1987@gmail.com>
2022-05-14 19:06:12 -0600

    category update route added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,7 +48,22 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id'})
+    }
+
+    res.status(200).json(categoryData)
   } catch (err) {
     res.status(500).json(categoryData)
   }
